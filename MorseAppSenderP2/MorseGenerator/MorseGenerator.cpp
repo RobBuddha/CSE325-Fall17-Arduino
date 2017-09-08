bda6190c97004dba787c22bd22c692ed544190fa
acshearer <acshear1@asu.edu>
2017-09-08 16:34:09 -0700

    typo

--- MorseAppSenderP2/MorseGenerator/MorseGenerator.cpp
+++ MorseAppSenderP2/MorseGenerator/MorseGenerator.cpp
@@ -43,7 +43,7 @@ void MorseGenerator::stringGen(String str)
   for (int i = 0; i < str.length(); i++) {
     char ch = str.charAt(i); // Store the value of the current character
     if (ch == '.'){
-      digitalWrite(_morsePin, HIGH);zz
+      digitalWrite(_morsePin, HIGH);
       return; // If current character is a period, end morse code translation
     }
     charGen(ch); // Output morse for the current character
