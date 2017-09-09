f45919bc03ce2f9231b17f370419cb0158ce4099
Erin Hintze <enhintze@asu.edu>
2017-09-08 17:44:19 -0700

    Comments updated to Interrupt

--- MorseAppSenderP2/MorseReaderInterrupt/MorseReader.cpp
+++ MorseAppSenderP2/MorseReaderInterrupt/MorseReader.cpp
@@ -1,13 +1,13 @@
 #include "Arduino.h"
 #include "MorseReader.h"
 
+// Declare variables
 int buttonPin;
 int button_s = 0;
 int last_b_s = 0;
 String ch;
-unsigned long lastChange = 0; //Keeps track of the 250ms spaces between letters, not using digitalRead()
-unsigned long curTime, dif;
-bool lastSpaceChar = false;
+unsigned long lastChange = 0; // Keeps track of the 250ms spaces between letters, not using digitalRead()
+unsigned long curTime, dif;   // Will
 
 MorseReader::MorseReader(int pin)
 {
@@ -17,6 +17,8 @@ MorseReader::MorseReader(int pin)
 
 void MorseReader::convert_morse(String str)
 {
+   // Morse to Alphanumeric conversion block.
+
   if (str == ".-") {
     Serial.print("A");
     }
@@ -126,33 +128,45 @@ void MorseReader::convert_morse(String str)
     Serial.print("0");
     }
 
-  ch = "";
+  ch = "";                              // Reset the string to receive the next letter.
 }
 
 void MorseReader::Read_by_interrupt(int value)
 {
-  
-  button_s = value;
-  curTime = millis();
-  dif = curTime - lastChange;
-  
+
+  button_s = value;                     // Set the current state to button_s.
+  curTime = millis();                   // Record time in ms.
+
+  dif = curTime - lastChange;           // Duration of the LOW signal.
+
+  // If the previous state was HIGH and is currently LOW, a space occurred.
   if(button_s == HIGH && last_b_s == LOW){
+
     if(dif > 745 && dif < 755){
-      //Letter space
+      // Letter space happened (t = 750ms). Just convert the Morse string.
       convert_morse(ch);
+
     } else if(dif > 995 && dif < 1005){
+      // Word space happened (t = 1000ms). First convert the character from Morse
+      // string, then print a space to denote the end of a word.
       convert_morse(ch);
       Serial.print(" ");
     }
+
+  // Button was just LOW, and now has received input.
   } else if(button_s == LOW && last_b_s == HIGH){
+
+    // Dot or Dash happened. Measure duration of signal to determine HIGH/LOW.
     if(dif > 245 && dif < 255){
+      // Dot happened (t=250ms).
       ch += '.';
     } else if(dif > 745 && dif < 755){
+      // Dash happened (t=750 ms).
       ch += '-';
     } else {
       convert_morse(ch);
     }
   }
-  lastChange = curTime;
-  last_b_s = button_s;
+  lastChange = curTime;         // Update the last_time with the current time.
+  last_b_s = button_s;          // Record the current state into the last state.
 }
