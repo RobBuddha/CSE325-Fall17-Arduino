123d1bb254506ec757e4a7b3ef3807b1a6b0b841
Erin Hintze <enhintze@asu.edu>
2017-10-06 17:10:08 -0700

    Comments added to Project 5 file

--- gpsNavigation_P5/Project_5/Project_5.ino
+++ gpsNavigation_P5/Project_5/Project_5.ino
@@ -9,11 +9,11 @@
 #include "DFR_Key.h"
 
 
-Adafruit_GPS GPS(&Serial1);                   // define GPS object connected to Serial 3
+Adafruit_GPS GPS(&Serial1);                    // define GPS object connected to Serial 3
 DFR_Key keypad;  
-Servo myservo;                                    // define servo object
-Adafruit_BNO055 bno = Adafruit_BNO055(55);        // define BNO sensor object
-LiquidCrystal lcd( 8, 9, 4, 5, 6, 7);             // define lcd pins use these default values for OUR LCD
+Servo myservo;                                 // define servo object
+Adafruit_BNO055 bno = Adafruit_BNO055(55);     // define BNO sensor object
+LiquidCrystal lcd( 8, 9, 4, 5, 6, 7);          // define lcd pins use these default values for OUR LCD
 
 #define GPSECHO  false
 
@@ -31,17 +31,17 @@ float Bearing = 0;                             // bearing angle to destination
 int localkey = 0;                              // variable for keypad
 int steerPrecision = 10;                       // Variable indicates the step size (in degrees) between steering angles.
 int carSpeed = 0;                              // speed of car
-int lastServoVal  = 0;
-int lastCarSpeed = 0;
+int lastServoVal  = 0;                         // holds last value written to servo
+int lastCarSpeed = 0;                          // holds last raw speed of the car
 float distance = 10;                           // distance from destination (in meters)
 bool readyToDrive = false;                     // State of the car
 int stopDistance = 5;                          // Distance from reference where car is allowed to stop
 int minSatellites = 5;                         // Minimum number of satellites required to for GPS to operate
 
 void setup() {
-  myservo.attach(44); // servo is connected to pin 44
-  lcd.begin( 16, 2 ); // LCD type is 16x2 (col & row)
-  Serial.begin(9600); // serial for monitoring
+  myservo.attach(44);                          // servo is connected to pin 44
+  lcd.begin( 16, 2 );                          // LCD type is 16x2 (col & row)
+  Serial.begin(9600);                          // serial for monitoring
 
   Serial.println("Orientation Sensor Calibration"); Serial.println("");
   if (!bno.begin(Adafruit_BNO055::OPERATION_MODE_NDOF)) {                               // If you want to calibrate using another mode, set it here.
@@ -77,7 +77,7 @@ void setup() {
   while (lat == 0 || lon == 0) { // No GPS signal yet
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("No GPS Signal");
+    lcd.print("No GPS Signal");                   // Alert user that GPS signal has not yet been obtained.
     delay(250);
   }
 
@@ -136,20 +136,22 @@ void useInterrupt(boolean v) {
   }
 }
 
-ISR(TIMER4_OVF_vect) { // This function will be called every 1 second (1Hz)
-  sei();        //   set interrupt flag // don't change this
-  TCNT4  = 336; //   re-initialize timer4's value
-  ReadGPS();    //   read GPS data
+ISR(TIMER4_OVF_vect) {                                  // This function will be called every 1 second (1Hz)
+  sei();                                                //   set interrupt flag
+  TCNT4  = 336;                                         //   re-initialize timer4's value
+  ReadGPS();                                            //   read GPS data
 }
 
-void ReadGPS() {
-  // read from GPS module and update the current position
+void ReadGPS() {                                        // read from GPS module and update the current position
+  
   if (GPS.newNMEAreceived()) {
-    GPS.parse(GPS.lastNMEA());
+    GPS.parse(GPS.lastNMEA());                          // Parse through location data collected from the GPS
   }
-  if (GPS.satellites >= minSatellites) {
-    lat = GPS.latitude;
+  if (GPS.satellites >= minSatellites) {                // Read in GPS data so long as 5 or more satellites are in reach
+    lat = GPS.latitude;                                 // More accurate if left in this notation rather than using <.latitudeDegrees>
     lon = GPS.longitude;
+
+    // Create a grid by denoting negative Quadrants.
     if (GPS.lon == 'W') {
       lon = lon * -1;
     }
@@ -164,9 +166,9 @@ void ReadHeading() {
   
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   
-  HEADING = euler.x(); // Grab euler direction output
+  HEADING = euler.x();                                  // Grab euler direction output
   
-  HEADING -= errorHeadingBearing; // Adds error in HEADING for Tempe. 
+  HEADING -= errorHeadingBearing;                       // Factors in error in HEADING for Tempe. 
   
   // Corrects any values which exceed 180 degrees.
   if (HEADING > 180) HEADING -= 360;
@@ -197,19 +199,17 @@ void CalculateBearing() {
   Bearing = (atan2(deltaLat, deltaLon) * 180/ PI) - 90; // OUTPUT: 0° is NORTH, -90° is EAST, -180° is SOUTH, 90/-270° is WEST.
 
   if (Bearing < -180) {
-    Bearing += 360;
-  } // OUTPUT: 0° is NORTH, -90° is EAST, +/-180° is SOUTH, 90° is WEST.
+    Bearing += 360;                           // OUTPUT: 0° is NORTH, -90° is EAST, +/-180° is SOUTH, 90° is WEST.
+  }
 
-  Bearing = Bearing * -1;
-  // FINAL OUTPUT: 0° is NORTH, 90° is EAST, +/-180° is SOUTH, -90° is WEST.
+  Bearing = Bearing * -1;                     // FINAL OUTPUT: 0° is NORTH, 90° is EAST, +/-180° is SOUTH, -90° is WEST.
 }
 
 void CalculateSteering() {                    // Calculate the steering angle according to the Bearing reference heading and actual heading.
   int leftBound, rightBound, backBound;       // Create variables to determine the bounds of <Bearing>. Range is (-150)-180.
   
-  if (Bearing < -90) {
-    // Between -180 and -90 (does not include -180 or -90). Given that Bearing will be in increments
-    // of 30 degrees, Bearing will specifically either be (-150) degrees or (-120) degrees.
+  if (Bearing < -90) {                        //     Between -180 and -90 (does not include -180 or -90). Given that Bearing will be in increments
+                                              // of 30 degrees, Bearing will specifically either be (-150) degrees or (-120) degrees.
     
     // Alter the bounds to accomodate for these angles.
     leftBound = Bearing + 270;
@@ -223,12 +223,13 @@ void CalculateSteering() {                    // Calculate the steering angle ac
     } else if (HEADING <= backBound) {                                                    // If the car is facing nearly opposite of the "desired
       STEERANGLE = 0;                                                                     // direction," but still on the left, start turning left
                                                                                           // until the next reading.
+                                                                                          
     } else {                                                                              // Else, start turning right until the next magnetometer
       STEERANGLE = 180;                                                                   // reading.
     }
-  } else if (Bearing < 0) {
-    // Between -90 and 0 (includes -90 but not 0). Given that Bearing will be in increments of 30 degrees,
-    // Bearing will specifically either be (-90, -60, -30) degrees.
+    
+  } else if (Bearing < 0) {                   //    Between -90 and 0 (includes -90 but not 0). Given that Bearing will be in increments of 30
+                                              // degrees, Bearing will specifically either be (-90, -60, -30) degrees.
   
     // Alter the bounds to accomodate for these angles.
     leftBound = Bearing - 90;
@@ -238,14 +239,15 @@ void CalculateSteering() {                    // Calculate the steering angle ac
     if (HEADING >= leftBound && HEADING <= rightBound) {                                  // If the car is between the left and right bounds,
       STEERANGLE = (int)((rightBound - HEADING)/steerPrecision) * steerPrecision;         // change the angle of steering by an increment of 10 degrees,
                                                                                           // so as to accurately drive in the "desired direction".
+                                                                                          
     } else if (HEADING <= backBound && HEADING >= rightBound) {                           // If the car is outside the back and right bounds, start
       STEERANGLE = 0;                                                                     // turning completely left until the next magnetometer reading.
+      
     } else {                                                                              // Else, the car starts turning completely right until the
       STEERANGLE = 180;                                                                   // next magnetometer reading.
     }
-  } else if (Bearing <= 90) {
-    // Between 0 and 90 (includes 0 and 90). Given that Bearing will be in increments of 30 degrees, Bearing will
-    // specifically either be (120, 150, 180) degrees.
+  } else if (Bearing <= 90) {                 //      Between 0 and 90 (includes 0 and 90). Given that Bearing will be in increments of 30 degrees, Bearing
+                                              // will specifically either be (120, 150, 180) degrees.
   
     // Alter the bounds to accomodate for these angles.
     leftBound = Bearing - 90;
@@ -263,9 +265,8 @@ void CalculateSteering() {                    // Calculate the steering angle ac
       STEERANGLE = 0;                                                                     // next magnetometer reading.
     }
   
-  } else if (Bearing <= 180) {
-    // Between 90 and 180 (includes 180 but not 90). Given that Bearing will be in increments of 30 degrees, Bearing
-    // will specifically either be (0, 30, 60, 90) degrees.
+  } else if (Bearing <= 180) {                //     Between 90 and 180 (includes 180 but not 90). Given that Bearing will be in increments
+                                              // of 30 degrees, Bearing will specifically either be (0, 30, 60, 90) degrees.
     
     // Alter the bounds to accomodate for these angles.
     leftBound = Bearing - 90;
@@ -278,6 +279,7 @@ void CalculateSteering() {                    // Calculate the steering angle ac
       STEERANGLE = (int)((rightBound - HEADING)/steerPrecision) * steerPrecision;
     } else if (HEADING > rightBound && HEADING <= backBound) {                            //     If the car is outside the right bound and the back bound,
       STEERANGLE = 0;                                                                     // start turning completely left until the next magnetometer reading.
+      
     } else {                                                                              //     Else, the car starts turning completely right until the next
       STEERANGLE = 180;                                                                   // magnetometer reading.
     }
@@ -288,37 +290,38 @@ void CalculateSteering() {                    // Calculate the steering angle ac
 }
 
 // Haversine formula
-void CalculateDistance() {
-  // Function calculates the distance between the car and the destination using coordinates from GPS (units = meters)
-  float r = 6371000; // Earth's radius in meters
-
+void CalculateDistance() {                    // Function calculates the distance between the car and the destination using coordinates from GPS (units = meters)
+  
+  float r = 6371000;                          // Earth's radius in meters
   float lat_rad = lat/100 * PI/180;
   float lon_rad = lon/100 * PI/180;
   float latDst_rad = latDestination/100 * PI/180;
   float lonDst_rad = lonDestination/100 * PI/180;
 
-  float deltaLat = latDst_rad - lat_rad;
-  float deltaLon = lonDst_rad - lon_rad;
+  float deltaLat = latDst_rad - lat_rad;      // Change in latitude
+  float deltaLon = lonDst_rad - lon_rad;      // Change in longitude
 
   float a = sin(deltaLat/2) * sin(deltaLat/2) + cos(lat_rad) * cos(latDst_rad) * sin(deltaLon/2) * sin(deltaLon/2);
   float c = 2 * atan2(sqrt(a), sqrt(1-a));
-  distance = r * c; // in meters
+  
+  distance = r * c;                           // in meters
 }
 
 void Actuate() { // Input: Steering angle - Output: nothing
+  
   if (readyToDrive) {
-    if (distance < stopDistance) {
-      // Stop car if distance from destination is less than stopDistance (meters)
+    
+    if (distance < stopDistance) {            // Stop car if distance from destination is less than stopDistance (meters)
       carSpeed = 0;
       readyToDrive = false;
     } else {
       carSpeed = 255*0.1;
     }
-    if(lastServoVal != STEERANGLE){
+    if(lastServoVal != STEERANGLE){           // If STEERING ANGLE has changed, set the new angle.
       myservo.write(STEERANGLE);
       lastServoVal = STEERANGLE;
     }
-    if(lastCarSpeed != carSpeed) {
+    if(lastCarSpeed != carSpeed) {            // If carSpeed has been changed, set the new carSpeed.
       analogWrite(carSpeedPin, carSpeed);
       lastCarSpeed = carSpeed;
     }
@@ -332,10 +335,10 @@ ISR(TIMER1_OVF_vect) {                            // This function will be calle
   CalculateBearing();                             // calc bearing
   CalculateSteering();                            // calc steering
   CalculateDistance();                            // calc distance
-  Actuate();                                      // Actuate
+  Actuate();                                      // only print to Actuate once
 }
 
-void printLocationOnLCD() {
+void printLocationOnLCD() {                       // Display 5 digits of the Latitude and Longitude coordinates on the LCD monitor.
   lcd.print("Lat: ");
   lcd.print(lat, 5);
   lcd.setCursor(0,1);
