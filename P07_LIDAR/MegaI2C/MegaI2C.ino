3dedf3ec4bf4df75ef779ea412800453ed81c1d8
acshearer <acshear1@asu.edu>
2017-10-31 14:23:18 -0700

    Start P07

---
+++ P07_LIDAR/MegaI2C/MegaI2C.ino
@@ -0,0 +1,188 @@
+#include <Adafruit_BNO055.h>
+#include <FlexiTimer2.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#include <utility/imumaths.h>
+#include <Servo.h>
+#include <Adafruit_GPS.h>
+#include "DFR_Key.h"
+
+Adafruit_GPS GPS(&Serial3);                   // define GPS object DO NOT USE Serial0
+DFR_Key keypad;                               // define keypad object
+Servo myservo;                                // define servo object
+Adafruit_BNO055 bno = Adafruit_BNO055(55);    // define BNO sensor object
+
+#define GPSECHO  false                        // echo GPS Sentence 
+#define Threshold 5                           // Threshold for Obstacle avoidance (number of obstacles)
+
+LiquidCrystal lcd( 8, 9, 4, 5, 6, 7); // define lcd pins use these default values for OUR LCD
+
+// Global variables that change across functions
+int STEERANGLE = 90;        // servo initial angle (range is 0:180)
+float HEADING = 0;          // Heading in degree
+int LidarRight;             // LIDAR left
+int LidarLeft;              // LIDAR right
+boolean usingInterrupt = false;
+int carSpeedPin = 2;              // pin for DC motor (PWM for motor driver). don't use other pins....
+float errorHeadingRef = 0;        // error
+long int lat;                     // GPS latitude in degree decimal * 100000   |     we multiply decimal degree by 100000 to convert it to meter  https://en.wikipedia.org/wiki/Decimal_degrees
+long int lon;                     // GPS latitude in degree decimal * 100000   |     0.00001 decimal degree is equal to 1.0247 m at 23 degree N/S
+long int latDestination = 33.425891 * 100000;       // define an initial reference Latitude of destination
+long int lonDestination =  -111.940458 * 100000;    // define an initial reference Longitude of destination
+float Bearing = 0;                                  // initialize bearing
+int localkey = 0;                                   // var
+
+void setup() {
+  myservo.attach(44);     // servo is connected to pin 44     (All pins are used by LCD except 2. Pin 2 is used for DC motor)
+  lcd.begin( 16, 2 );     // LCD type is 16x2 (col & row)
+
+  ///Setting the reference (Lat and Lon)///
+  localkey = 0;
+  while (localkey != 1) {
+    lcd.clear();
+    localkey = keypad.getKey();
+    lcd.print("Press Select");
+    lcd.setCursor(0, 1);
+    lcd.print("to save dest.");
+    delay(100);
+  }
+  GPSRead();
+  latDestination = lat;     // saving the destiantion point
+  lonDestination = lon;     // saving the destiantion point
+  localkey = 0;
+  while (localkey != 1) {
+    lcd.clear();
+    localkey = keypad.getKey();
+    lcd.print("Press Select");
+    lcd.setCursor(0, 1);
+    lcd.print("to drive!");
+    delay(100);
+  }
+
+
+  Serial.begin(9600);     // serial for monitoring
+  if (!bno.begin(Adafruit_BNO055::OPERATION_MODE_NDOF)) {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+  byte c_data[22] = {0, 0, 0, 0, 0, 0, 209, 4, 9, 5, 9, 6, 0, 0, 255, 255, 255, 255, 232, 3, 1, 3};               // Use your CALIBRATION DATA
+  bno.setCalibData(c_data);                                                                                       // SET CALIBRATION DATA
+  bno.setExtCrystalUse(true);
+
+  // set timer interrupts
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;               // initialize timer1
+  TCCR1B = 0;               // initialize timer1
+  TCNT1  = 59016;           // interrupt is generated every 0.1 second ( ISR(TIMER1_OVF_vect) is called)
+  TCCR1B |= (1 << CS12);    // 256 prescaler
+  TIMSK1 |= (1 << TOIE1);   // enable timer compare interrupt
+
+  TCCR4A = 0;               // initialize timer4
+  TCCR4B = 0;               // initialize timer4
+  TCNT4  = 336;             // interrupt is generated every 1 second  ( ISR(TIMER4_OVF_vect) is called)
+  TCCR4B |= (1 << CS42);    // 256 prescaler
+  TIMSK4 |= (1 << TOIE4);   // enable timer compare interrupt
+  interrupts();             // enable intrrupt flag again
+
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);   // ask GPS to send RMC & GGA sentences
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate, don't use higher rates
+  GPS.sendCommand(PGCMD_ANTENNA);               // notify if antenna is detected
+  useInterrupt(true);                           // use interrupt for reading chars of GPS sentences (From Serial Port)
+}
+
+SIGNAL(TIMER0_COMPA_vect) {       // don't change this !!
+  char c = GPS.read();            // interrupt for reading GPS sentences char by char....
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;
+#endif
+}
+
+void useInterrupt(boolean v) {    // enable inttrupt for GPS, don't change this!!
+  if (v) {
+    OCR0A = 0xAF;               // Timer0 is already used for millis() - we'll just interrupt somewhere
+    TIMSK0 |= _BV(OCIE0A);      // in the middle by Output Compare Register A (OCR0A) and "TIMER0_COMPA_vect" function is called
+    usingInterrupt = true;
+  } else {
+    TIMSK0 &= ~_BV(OCIE0A); // do not call the interrupt function COMPA anymore
+    usingInterrupt = false;
+  }
+}
+
+ISR(TIMER4_OVF_vect) {  // Timer interrupt for reading GPS DATA
+  sei();        //   reset interrupt flag
+  TCNT4  = 336; //   re-initialize timer value
+  GPSRead();    //   read GPS data
+}
+
+void GPSRead() {
+  // read GPS data
+}
+
+
+void ReadHeading()
+{
+  // calculate HEADING
+}
+
+void CalculateBearing() {
+  // Calculate Bearing
+}
+
+void CalculateSteer() {
+  // Calculate Steer angle based on GPS data and IMU
+}
+
+void SetCarDirection() {    // Input: Lidar data
+  // Set Steering angle,
+  // If any obstacle is detected by Lidar, Ignore steering angle and turn left or right based on observation
+}
+
+void SetCarSpeed() {  // Input: GPS data
+  // set speed,
+  // if destination is within 5 meters of current position, set speed to zero.
+}
+
+void ReadLidar() {    // Output: Lidar Data
+  // read Lidar Data from Nano Board (I2C)
+  // you should request data from Nano and read the number of obstacle (within the range) on your rightside and leftside
+  // Then, you can decide to either do nothing, turn left or turn right based on threshold. For instance, 0 = do nothing, 1= left and 2 = right
+
+}
+
+ISR(TIMER1_OVF_vect) {        // function will be call every 0.1 seconds
+  sei();                  // reset interrupt flag
+  TCNT1  = 59016;
+  ReadHeading();
+  CalculateBearing();
+  CalculateSteer();
+  SetCarDirection();
+  SetCarSpeed();
+}
+
+
+void printHeadingOnLCD() {
+
+}
+
+void printLocationOnLCD() {
+
+}
+
+void printDistanceOnLCD() {
+
+}
+
+void printObstacleOnLCD() {
+
+}
+
+void loop() {
+  lcd.clear();      // clear lcd
+  // Pring data to LCD in order to debug your program!
+  printHeadingOnLCD();
+  printObstacleOnLCD();
+  delay(100);
+}
---
+++ P07_LIDAR/NanoI2C/NanoI2C.ino
@@ -0,0 +1,53 @@
+#include <Wire.h>
+#include <RPLidar.h>
+RPLidar lidar;          // define lidar as RPLIDAR Object
+#define RPLIDAR_MOTOR 3 // motor pin for lidar speed control
+
+void setup() {
+  pinMode(RPLIDAR_MOTOR, OUTPUT); // set lidar speed control pin as output
+  lidar.begin(Serial);            // begin communication with lidar
+  Wire.begin(8);                  // join i2c bus with address 8 as SLAVE
+  Wire.onRequest(requestEvent);   // call "requestEvent" function when receives a request
+  Wire.onReceive(receiveEvent);   // call "receiveEvent" function when receives a byte
+}
+int left = 0;                     // variable for detected points on left hand side
+int right = 0;                    // variable for detected points on right hand side
+unsigned long time = millis();    // time variable for reseting variables
+
+int c1;                           // variable for received integer
+
+void receiveEvent(int bytes)
+{
+       // read the received byte as integer. This indicates what data to send back when master is requesting data
+}
+
+void requestEvent() 
+{
+   // receive message byte as a character
+   // if master's request is right side data, ("1"), send back the right side data
+   // if master's request is left side data, ("2"), send back the left side data
+  
+}
+
+void loop() 
+{
+  if (IS_OK(lidar.waitPoint())) { // if lidar is working properly (waiting time less than timeout)
+    // read angle and distance of the obstacle
+    // filter data (keep only the data in desired range and with desired angle) 
+    // COUNT the number of obstacles on LEF and RIGHT side
+    // reset obstacle variables every 1 second
+
+
+    
+  } else {                                                  // if lidar is not responding           // Dont change this......
+    analogWrite(RPLIDAR_MOTOR, 0);                          //stop the rplidar motor                // Dont change this......
+    rplidar_response_device_info_t info;                    // try to detect RPLIDAR...             // Dont change this......
+    if (IS_OK(lidar.getDeviceInfo(info, 100))) {            // if detected,                         // Dont change this......
+      lidar.startScan();                                    // start scan                           // Dont change this......
+      analogWrite(RPLIDAR_MOTOR, 255);                      // start motor rotating at max speed    // Dont change this......
+      delay(1000);                                                                                  // Dont change this......
+    }
+  }
+}
+
+
